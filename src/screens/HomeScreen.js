121a056ee9bc0d7583bf734a13c5d413a92dc137
YifanWang97 <yifan@lobbie.com>
2022-02-12 01:09:19 -0800

    setup axios get response from backend

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Row, Col} from 'react-bootstrap';
 import Product from '../components/Product';
-import products from '../products';
+import Axios from 'axios';
 
 function HomeScreen() {
+
+  // initialize products array to empty state 
+  const [products, setProducts] = useState([]);
+
+  // useEffect gets triggered when a component loads
+  // or when a state attribute is changed
+  // useEffect needs to be triggered only when component first load
+  useEffect(() => {
+    // wrap wait inside an async function
+    async function fetchProducts() {
+      // use wait to return the promise
+      // destructure into data
+      const {data} = await Axios.get('http://localhost:8000/api/products/');
+      setProducts(data);
+    }
+
+    fetchProducts()
+    
+  }, [])
   return (
     <div>
         <h1>Latest Products</h1>
